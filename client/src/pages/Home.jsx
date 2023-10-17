53e6de8e753be44afeb1e6d17a87d1b9b6afd0e1
JDunc <jjsduncan@gmail.com>
2023-10-17 17:58:58 -0400

    setup client and server folders

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_MATCHUPS } from '../utils/queries';
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_MATCHUPS, {
+    fetchPolicy: "no-cache"
+  });
+
+  const matchupList = data?.matchups || [];
+
+  return (
+    <div className="card bg-white card-rounded w-50">
+      <div className="card-header bg-dark text-center">
+        <h1>Choose Your Adventure!</h1>
+      </div>
+      <div className="card-body m-5">
+        <h2>A reader guided story with many outcomes</h2>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ul className="square">
+            {matchupList.map((matchup) => {
+              return (
+                <li key={matchup._id}>
+                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
+                    {matchup.tech1} vs. {matchup.tech2}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+      <div className="card-footer text-center m-3">
+        <h2>Work in Progress</h2>
+        <Link to="/matchup">
+          {/* <button className="btn btn-lg btn-danger">Create Matchup!</button> */}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
